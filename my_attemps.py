78fca32c93d002654641b0a817018742eb712553
Rutabana <rutabana12loic@gmail.com>
2023-12-29 10:18:15 -0500

    Ability to find and set scale factrs added. Better scale factor validation and outter frame expands with long file names now

--- my_attemps.py
+++ my_attemps.py
@@ -3,15 +3,22 @@
 from tkinter import filedialog
 import customtkinter as ctk
 import os
+import array as arr
+
+# TODO: Add a button to remove a file entry
+# TODO: Add a button to clear all file entries
+# TODO: Remember the last file path used
+# TODO: Add a button to add a folder
+# TODO: Make it possible to change a file entry
+# TODO: A comments to method
 
 
 class LogFile:
-  def __init__(self, filename, pathname, first_time, first_open, scale_factor):
+  def __init__(self, filename, pathname, first_time, first_open):
     self.filename = filename
     self.pathname = pathname
     self.first_time = first_time
     self.first_open = first_open
-    self.scale_factor = scale_factor
 
 def create_ui():
   ctk.set_appearance_mode("dark")
@@ -26,12 +33,41 @@ def create_ui():
 
   max_files = 10
 
+  # GUI
+  frame = ctk.CTkScrollableFrame(root, width=1000, height=1000)
+  frame.pack(pady=70, padx=70)
+  frame.pack_propagate(True)
+
   # LOGIC
 
-  # utility to insure that user enters a value numeric
-  def is_number(s):
+   # Returns true if the string contains the substring
+  def contains(str, sub):
+    return str.find(sub) != -1
+  
+  # Utility function to find the scale factor of a given file entry number
+  def find_scale_factor(i):
+    frame_name = f'!ctkframe{i}' if i != 1 else '!ctkframe'
+    frame_in_question = frame.children.get(frame_name)
+    placeholder_text = frame_in_question.children.get("!ctkentry").cget("placeholder_text")
+    val = frame_in_question.children.get("!ctkentry").get()
+    print(f'placeholder_text: {placeholder_text}, val: {val}')
+    if val == "" or contains(val, "scale factor"):
+      return 1
+    if val == "-":
+      return -1
+    return val
+  
+  #  Utility function to set a scale factor of a given file entry number
+  def set_scale_factor(i, val):
+    frame_name = f'!ctkframe{i}' if i != 1 else '!ctkframe'
+    frame_in_question = frame.children.get(frame_name)
+    frame_in_question.children.get("!ctkentry").delete(0, "end")
+    frame_in_question.children.get("!ctkentry").insert(0, val)
+    
+  # utility to insure that user enters a value numeric (decimcal)
+  def scale_factor_validator(s):
       try:
-        if (s==""):
+        if (s=="" or contains(s, "scale factor") or s=='-'):
           return True
         float(s)
         return True
@@ -39,23 +75,16 @@ def create_ui():
           return False
   
   def file_explorer():
-    path = filedialog.askopenfilename(initialdir = "/" ,title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
+    path = filedialog.askopenfilename(initialdir = "/Users/rutab/Desktop/Workspace/Physics-Research/", title = "Select file",filetypes = (("dat files","*.dat"),("all files","*.*")))
     if (path != ""):
       logfiles.append(path)
       filename = os.path.basename(path)
+      log_file = LogFile(filename, os.path.dirname(path), True, True)
       labels[len(logfiles)-1].configure(text=filename, text_color="#0066bf")
-
-  def read():
-    print("reading file")
-
-  # GUI
-  frame = ctk.CTkScrollableFrame(root, width=550, height=1000)
-  frame.pack(pady=10, padx=10)
-  frame.pack_propagate(False)
 
   inner_frame = ctk.CTkFrame(frame, width=500, height=70, fg_color="transparent")
   inner_frame.grid(pady=10, padx=10, column=0, row=1)
-  # inner_frame.grid_propagate(False)
+  inner_frame.grid_propagate(True)
 
   path_entry = ctk.CTkEntry(frame, width=500, placeholder_text="Path to file/folder")
   path_entry.grid(pady=30, padx=10, column=0, row=0)
@@ -64,13 +93,33 @@ def create_ui():
   fil1_btn.grid(pady=10, padx=10, column=1, row=0)
 
   # Create a Tcl wrapper for the validation command. It makes sure input is numeric
-  vcmd = (root.register(is_number), '%P')
-  sc1_btn = ctk.CTkEntry(inner_frame, width=70, height=50, validate="key", validatecommand=vcmd)
-  sc1_btn.grid(pady=10, padx=10, column=2, row=0)
+  vcmd = (root.register(scale_factor_validator), '%P')
+  sc1_entry = ctk.CTkEntry(inner_frame, width=100, height=50, validate="key", validatecommand=vcmd, placeholder_text="scale factor 1")
+  sc1_entry.grid(pady=10, padx=10, column=2, row=0)
 
   file1_name_lbl = ctk.CTkLabel(inner_frame, text=f'File {file_count} Name')
   file1_name_lbl.grid(pady=10, column=3, row=0)
   labels.append(file1_name_lbl)
+
+  # Reads file (Only handles unsiged integers)
+  def read(filename):
+    # Check if it's the first time the file is being read
+    # TODO: Handle this case
+
+    # Log message indicating the file is being read
+    print("Reading file for the first time")
+
+    # Initialize spectrum array
+    spectrum = arr.array('I', [])
+    # Open the file
+    # Initialize vairables file_channel_count , number_full_lines and remainder
+
+    # Loop over each file, updating the spectrum array accordingly
+
+    # Handle the remainder
+
+    # Print out the spectrum array
+
 
   file1_read_btn = ctk.CTkButton(inner_frame, text="Read", command=read, width=70, height=50)
   file1_read_btn.grid(pady=10, padx=10, column=4, row=0)
@@ -90,9 +139,8 @@ def create_ui():
         fil_btn = ctk.CTkButton(new_frame, text=f'File {file_count}', command=file_explorer, width=70, height=50)
         fil_btn.grid(pady=10, padx=10, column=1, row=0)
 
-
-        sc_btn = ctk.CTkEntry(new_frame, width=70, height=50, validate="key", validatecommand=vcmd)
-        sc_btn.grid(pady=10, padx=10, column=2, row=0)
+        sc_entry = ctk.CTkEntry(new_frame, width=70, height=50, validate="key", validatecommand=vcmd, placeholder_text=f'scale factor {file_count}')
+        sc_entry.grid(pady=10, padx=10, column=2, row=0)
 
         file_name_lbl = ctk.CTkLabel(new_frame, text=f'File {file_count} Name')
         file_name_lbl.grid(pady=10, column=3, row=0)
@@ -130,7 +178,10 @@ def create_ui():
 
   # Custom close function
   def on_close():
+    nonlocal root
     print("exiting")
+    print(find_scale_factor(1))
+    root.destroy()
     exit()
   #     print("exiting ", logfilename)
   #     check_file = os.path.isfile(logfilename)
